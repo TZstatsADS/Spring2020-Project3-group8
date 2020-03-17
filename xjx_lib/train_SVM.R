8202f911787f2abdb7d73292abf72f4dddcf2d19
JinxuXiang <jx2399@columbia.edu>
2020-03-16 20:45:04 -0400

    update

--- xjx_lib/train_SVM.R
+++ xjx_lib/train_SVM.R
@@ -1,11 +1,12 @@
 ###########################################################
 ### Train a classification model with training features ###
 ###########################################################
-svm_train <- function(feature_df = pairwise_data, cost = 1, probability = FALSE, kernel = 'linear', degree = 3){
+svm_train <- function(feature_df = pairwise_data, cost = 1, probability = FALSE, kernel = 'linear', degree = 3, gamma = 1/ncol(feature_df)){
 
   tm.train = system.time(
     svm_model <- svm(emotion_idx ~ ., data = feature_df,
-                     kernel = kernel, cost = cost, probability=probability, degree = degree) 
+                     kernel = kernel, cost = cost, probability=probability, 
+                     degree = degree, gamma = gamma) 
   )
   return(list(model = svm_model, tm.train))
 }
