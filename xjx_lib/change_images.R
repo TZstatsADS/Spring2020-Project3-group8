094580a252f66eb22cc0f95ab2b6730e22c02a00
JinxuXiang <jx2399@columbia.edu>
2020-03-18 11:18:47 -0400

    update

--- xjx_lib/change_images.R
+++ xjx_lib/change_images.R
@@ -7,7 +7,7 @@ load("../output/fiducial_pt_list.RData")
 single = c(35,36,37,38,44,52,56,59,62)
 double = data.frame(x1 = c(1,2,3,4,5,6,7,8,9,19,20,21,22,23,24,25,26,39,40,41,42,43,50,51,57,58,63),
                     x2 = c(10,15,14,13,12,11,18,17,16,31,30,29,28,27,34,33,32,49,48,47,46,45,54,53,55,60,61))
-fixdist = -170
+
 
 cal_angle = function(pos){
   model = lm(x ~ y, data = pos)
@@ -17,29 +17,20 @@ cal_angle = function(pos){
   return(angle)
 }
 
-cal_rotation = function(points, angle){
-  points_rotation = data.frame(x = NULL, y = NULL)
-  for(i in 1:78){
-    x = points[i,1] - 0
-    y = points[i,2] - 0
-    alpha = atan(y/x)
-    d = sqrt(x^2+y^2)
-    newx = d*cos(alpha + angle) + 0 + 750*sin(angle)
-    newy = d*sin(alpha + angle) + 0
-    
-    points_rotation = rbind(points_rotation, data.frame(x = newx, y = newy))
-  }
+cal_rotation = function(points, angle, center_x = 0, center_y = 0, move_x = 750*sin(angle), move_y = 0){
+  alpha=atan((points[,2]-center_y)/(points[,1]-center_x))
+  d=sqrt((points[,2]-center_y)^2+(points[,1]-center_x)^2)
+  newx=d*cos(alpha + angle) + center_x + move_x
+  newy=d*sin(alpha + angle) + center_y + move_y
+  points_rotation=data.frame(x=newx,y=newy)
   
   return(points_rotation)
 }
 
 cal_translate = function(points, center_x = 500, center_y = 375){
-  points_translate = data.frame(x = NULL, y = NULL)
-  for(i in 1:78){
-    newx = points[i,1] + (center_x - points[37,1])
-    newy = points[i,2] + (center_y - points[37,2])
-    points_translate = rbind(points_translate, data.frame(x = newx, y = newy))
-  }
+  newx=points[,1]+center_x-points[37,1]
+  newy=points[,2]+center_y-points[37,2]
+  points_translate=data.frame(x = newx, y = newy)
   
   return(points_translate)
 }
@@ -52,12 +43,11 @@ cal_zoom = function(points, rate){
   points*unlist(rate)
 }
 
-move_images = function(points, img, center_x = 500, center_y = 375){
-  pos = data.frame(x = points[single,1], y = points[single,2])
-  for(i in 1:nrow(double)){
-    pos = rbind(pos, data.frame(x = (points[double[i,1],1]+points[double[i,2],1])/2, 
-                                y = (points[double[i,1],2]+points[double[i,2],2])/2))
-  }
+move_images = function(points, img, center_x = 500, center_y = 375, fixdist = -170){
+  pos_single = data.frame(x = points[single,1], y = points[single,2])
+  pos_double=data.frame(x=0.5*points[double[,1],1]+0.5*points[double[,2],1],
+                        y=0.5*points[double[,1],2]+0.5*points[double[,2],2])
+  pos=rbind(pos_single,pos_double)
   
   angle = cal_angle(pos)
   points_rotation = cal_rotation(points, angle)
@@ -89,12 +79,11 @@ change_image = function(indices = c(1,2)){
 }
 
 # Only points no images
-move_points = function(points, center_x = 500, center_y = 375){
-  pos = data.frame(x = points[single,1], y = points[single,2])
-  for(i in 1:nrow(double)){
-    pos = rbind(pos, data.frame(x = (points[double[i,1],1]+points[double[i,2],1])/2, 
-                                y = (points[double[i,1],2]+points[double[i,2],2])/2))
-  }
+move_points = function(points, center_x = 500, center_y = 375, fixdist = -170){
+  pos_single = data.frame(x = points[single,1], y = points[single,2])
+  pos_double=data.frame(x=0.5*points[double[,1],1]+0.5*points[double[,2],1],
+                        y=0.5*points[double[,1],2]+0.5*points[double[,2],2])
+  pos=rbind(pos_single,pos_double)
   
   angle = cal_angle(pos)
   points_rotation = cal_rotation(points, angle)
