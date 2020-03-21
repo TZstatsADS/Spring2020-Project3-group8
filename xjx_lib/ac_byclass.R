59c80d8252fe9d222939ce769a0d184d42141e93
JinxuXiang <jx2399@columbia.edu>
2020-03-21 10:28:34 -0400

    update

--- xjx_lib/ac_byclass.R
+++
@@ -1,9 +0,0 @@
-ac_byclass <- function(conf_table){
-  table <- as.matrix(conf_table)
-  n <- dim(table)[2]
-  rate <- rep(NA, n)
-  for (i in 1:n){
-    rate[i] <- table[i,i]/colSums(table)[i]
-  }
-  return(rate)
-}
